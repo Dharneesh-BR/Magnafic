c6a651f6079553f574b9fa0fde7bb871ea6c17c6
Adithya N Kashyap <balajiadithya2003@gmail.com>
2026-05-28 17:05:23 +0530

    Update all recent changes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyAXtzTKDpcov18wCDP5RdEYVMRSoldB0VE',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'magnafic-3eddc.firebaseapp.com',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'magnafic-3eddc',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'magnafic-3eddc.firebasestorage.app',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '1098083884888',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:1098083884888:web:9293464bed2394e441cf2d',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || 'G-LM93XTJTZG',
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
